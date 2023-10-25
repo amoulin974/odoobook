87d551d677094addef98af5114bc114e3f31b5dc
amoulin006 <a.moulin@unistra.fr>
2023-10-25 17:05:38 +0200

    correction exo2

--- addons/library/__manifest__.py
+++ addons/library/__manifest__.py
@@ -3,25 +3,25 @@
     'name': "library",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Application de gestion de livre""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Antoine Moulin",
+    'website': "c2i-revision.fr",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'services/library',
+    'version': '16.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
-
+    'application': True,
+    'license': "AGPL-3",
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
